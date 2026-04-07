0e23d3ed842c96546548f14ae845a3cd478a200d
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-04-07 23:31:22 +0530

    Adding some features in Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const App = () => {
   return (<div>
     <h1 className='text-red-700 font-bold text-lg'>Anay Shrivastava</h1>
+    <p className='text-green-800 font-bold text-xl'>Hey Anay Bro How are men what are you doing today</p>
   </div>)
 
 }
