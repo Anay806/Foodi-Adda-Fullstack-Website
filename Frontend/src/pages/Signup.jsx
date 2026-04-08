3d7361522fa7fb65085222e279eb92b9c76d2ed7
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-04-08 22:08:17 +0530

    Adding something new

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -14,6 +14,8 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Eye, EyeOff } from 'lucide-react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 
 
@@ -21,8 +23,48 @@ import { Eye, EyeOff } from 'lucide-react'
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  })
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    console.log(formData);
+    try {
+      const res = await axios.post('http://localhost:8000/api/v1/user/register', formData, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+
+      if (res.data.success) {
+        navigate('/verify')
+      }
+
+
+    } catch (error) {
+      console.log(error);
+
+
+    }
+
+  }
   return (
-    <div className=' flex justify-center items-center min-h-screen bg-pink-100'>
+    <div className=' flex justify-center items-center min-h-screen bg-orange-100'>
 
       <Card className="w-full max-w-sm">
         <CardHeader>
@@ -33,58 +75,76 @@ const Signup = () => {
 
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-3">
-              <div className="grid grid-cols-2 gap-4">
-                <div className='grid gap-2'>
-                  <Label htmlFor="firstName">First Name</Label>
-                  <Input id='firstName' name='firstName' type='text' placeholder=' firstName' required ></Input>
-                </div>
-                <div className='grid gap-2'>
-                  <Label htmlFor="lastName">Last Name</Label>
-                  <Input id='lastName' name='lastName' type='text' placeholder=' lastName' required ></Input>
-                </div>
 
+          <div className="flex flex-col gap-3">
+            <div className="grid grid-cols-2 gap-4">
+              <div className='grid gap-2'>
+                <Label htmlFor="firstName">First Name</Label>
+                <Input
+                  id='firstName'
+                  name='firstName'
+                  type='text'
+                  placeholder=' firstName'
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  required ></Input>
               </div>
               <div className='grid gap-2'>
-                <Label htmlFor="email">Email</Label>
-
+                <Label htmlFor="lastName">Last Name</Label>
                 <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="m@example.com"
+                  id='lastName'
+                  name='lastName'
+                  type='text'
+                  placeholder=' lastName'
+                  required
+                  value={formData.lastName}
+                  onChange={handleChange} ></Input>
+              </div>
+
+            </div>
+            <div className='grid gap-2'>
+              <Label htmlFor="email">Email</Label>
+
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+
+              </div>
+              <div className='relative'>
+                <Input
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Creatyet a password"
+                  type={showPassword ? "text" : "password"}
                   required
                 />
+                {
+                  showPassword ? <EyeOff onClick={() => setShowPassword(false)} className='w-5 h-5 text-gray-700 absolute right-5 bottom-2' /> : <Eye onClick={() => setShowPassword(true)} className='w-5 h-5 text-gray-700 absolute right-5 bottom-2' />
+                }
               </div>
 
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-
-                </div>
-                <div className='relative'>
-                  <Input
-                    id="password"
-                    name="password"
-                    placeholder="Creatyet a password"
-                    type={showPassword ? "text" : "password"}
-                    required
-                  />
-                  {
-                    showPassword ? <EyeOff onClick={() => setShowPassword(false)} className='w-5 h-5 text-gray-700 absolute right-5 bottom-2' /> : <Eye onClick={() => setShowPassword(true)} className='w-5 h-5 text-gray-700 absolute right-5 bottom-2' />
-                  }
-                </div>
-
-              </div>
             </div>
-          </form>
+          </div>
+
         </CardContent>
         <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
-            SignUp
+          <Button onClick={submitHandler} type="submit" className="w-full cursor-pointer bg-orange-600 hover: bg-orange-500 ">
+            Signup
           </Button>
-
+          <p className='text-gray-700 text-sm '>Already have an account? <Link to={'/login'} className='hover:underline cursor-pointer text-orange-600 font-bold'>Login</Link></p>
         </CardFooter>
       </Card>
     </div>
