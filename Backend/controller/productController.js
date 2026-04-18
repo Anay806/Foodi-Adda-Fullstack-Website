1106df8ee7034121650c9af62f206991c631161b
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-04-18 12:44:29 +0530

    adding product and his credientls

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -80,3 +80,107 @@ export const getAllProduct = async (_, res) => {
 
   }
 }
+
+export const deleteProducts = async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const product = await Product.findById(productId)
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      })
+    }
+
+    //Delete images from cloudinary
+    if (product.productImg && product.productImg.length > 0) {
+      for (const img of product.productImg) {
+        const result = await cloudinary.uploader.destroy(img.public_id)
+
+      }
+    }
+
+    //delete product from mongoDB
+    await Product.findByIdAndDelete(productId);
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted successfully"
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    })
+
+  }
+}
+
+export const updateProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const { productName, productDesc, productPrice, category, brand, existingImages } = req.body;
+
+    const product = await Product.findById(productId)
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      })
+    }
+
+    let updatedImages = [];
+
+    //keep selected oldimages
+    if (existingImages) {
+      const keepIds = JSON.parse(existingImages);
+      updatedImages = product.productImg.filter((img) => keepIds.includes(img.public_id));
+
+      //delete only removed images
+      const removedImages = product.productImg.filter((img) => !keepIds.includes(img.public_id));
+
+      for (let img of removedImages) {
+        await cloudinary.uploader.destroy(img.public_id)
+      }
+    } else {
+      updatedImages = product.productImg;
+    }
+
+    //upload new images if any
+    if (req.files && req.files.length > 0) {
+      for (let file of req.files) {
+        const fileUri = getDataUri(file)
+        const result = await cloudinary.uploader.upload(fileUri, { folder: "mern_products" })
+        updatedImages.push({
+          url: result.secure_url,
+          public_id: result.public_id
+
+        })
+
+      }
+
+      //update product details
+
+      product.productName = productName || product.productName;
+      product.productDesc = productDesc || product.productDesc;
+      product.productPrice = productPrice || product.productPrice;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.productImg = updatedImages;
+
+      await product.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Product updated successfully",
+        product
+      })
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
