70166f3bc0a2279e123623034d27de3fcb4cb82f
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-04-27 15:33:32 +0530

    Adidng cart item increase and decrease functionalty

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -1,80 +1,137 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import logo from "../assets/logo.jpeg"
 import { Button } from '@/components/ui/button';
-import { Trash2 } from 'lucide-react';
+import { ShoppingCart, Trash2 } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
+import { Input } from '@/components/ui/input';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { setCart } from '@/redux/productSlice';
 
 const Cart = () => {
   const { cart } = useSelector(store => store.product)
   console.log(cart);
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const subtotal = cart?.totalPrice
   const shipping = subtotal > 299 ? 0 : 10;
   const tax = subtotal * 0.05 // 5%
   const total = subtotal + shipping + tax
 
+  const API = "http://localhost:8000/api/v1/cart"
+  const accessToken = localStorage.getItem("accessToken")
+
+  const handleUpdateQuantity = async (productId, type) => {
+    try {
+      const res = await axios.put(`${API}/update`, { productId, type }, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      if (res.data.success) {
+        dispatch(setCart(res.data.cart))
+      }
+
+    } catch (error) {
+      console.log(error);
+
+
+    }
+  }
+
   return (
     <div className='pt-20 bg-gray-50 min-h-screen'>
       {
-        cart?.items?.length > 0 ? <div className='max-w-7xl mx-auto'>
-          <h1 className='text-2xl font-bold text-gray-800 mb-7'>Shopping Cart</h1>
-          <div className='max-w-7xl mx-auto flex gap-7'>
-            <div className='flex flex-col gap-5 flex-1'>
-              {
-                cart?.items?.map((product, index) => {
-                  return <Card key={index}>
-                    <div className='flex justify-between items-center pr-7'>
-                      <div className='flex items-center gap-10 ml-3  w-[350px]'>
-                        <img src={product?.productId?.productImg?.[0]?.url || logo} alt="" className='w-25 h-25' />
-                        <div className='w-[280px] ml-10'>
-                          <h1 className='font-semibold truncate '>
-                            {product?.productId?.productName}</h1>
-                          <p className='font-semibold'>₹{product?.productId?.productPrice}</p>
+        cart?.items?.length > 0 ?
+          <div className='max-w-7xl mx-auto'>
+            <h1 className='text-2xl font-bold text-gray-800 mb-7'>Shopping Cart</h1>
+            <div className='max-w-7xl mx-auto flex gap-7'>
+              <div className='flex flex-col gap-5 flex-1'>
+                {
+                  cart?.items?.map((product, index) => {
+                    return <Card key={index}>
+                      <div className='flex justify-between items-center pr-7'>
+                        <div className='flex items-center gap-10 ml-3  w-[350px]'>
+                          <img src={product?.productId?.productImg?.[0]?.url || logo} alt="" className='w-25 h-25' />
+                          <div className='w-[280px] ml-10'>
+                            <h1 className='font-semibold truncate '>
+                              {product?.productId?.productName}</h1>
+                            <p className='font-semibold'>₹{product?.productId?.productPrice}</p>
+                          </div>
+
                         </div>
+                        <div className='flex gap-5 items-center'>
+                          <Button onClick={() => handleUpdateQuantity(product.productId._id, 'decrease')} variant='outline'>-</Button>
+                          <span>{product.quantity}</span>
+                          <Button onClick={() => handleUpdateQuantity(product.productId._id, 'increase')} variant='outline'>+</Button>
+                        </div>
+                        <p>{(product?.productId?.productPrice) * (product?.quantity)}</p>
+                        <p className='flex text-red-500 items-center gap-1 cursor-pointer'><Trash2 className='w-4 h-4' />Remove</p>
+                      </div>
 
-                      </div>
-                      <div className='flex gap-5 items-center'>
-                        <Button variant='outline'>-</Button>
-                        <span>1</span>
-                        <Button variant='outline'>+</Button>
-                      </div>
-                      <p>{(product?.productId?.productPrice) * (product?.quantity)}</p>
-                      <p className='flex text-red-500 items-center gap-1 cursor-pointer'><Trash2 className='w-4 h-4' />Remove</p>
+                    </Card>
+                  })
+                }
+
+              </div>
+              <div>
+                <Card className="w-[400px]">
+                  <CardHeader>
+                    <CardTitle>
+                      Order Sumarry
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent className="space-y-4">
+                    <div className='flex justify-between'>
+                      <span>Subtotal ({cart?.items?.length} items)</span>
+                      <span>₹{cart?.totalPrice?.toLocaleString('en-IN')}</span>
                     </div>
+                    <div className='flex justify-between'>
+                      <span>Shipping</span>
+                      <span>₹{shipping}</span>
+                    </div>
+                    <div className='flex justify-between'>
+                      <span>Tax(5%)</span>
+                      <span>₹{tax}</span>
+                    </div>
+                    <Separator />
+                    <div className='flex justify-between font-bold text-lg'>
+                      <span>Total</span>
+                      <span>₹{total}</span>
 
-                  </Card>
-                })
-              }
+                    </div>
+                    <div className='space-y-3 pt-4'>
+                      <div className='flex space-x-2'>
+                        <Input placeholder="Promo Code" />
+                        <Button className="bg-orange-600 cursor-pointer">Apply</Button>
+                      </div>
+                      <Button className="w-full bg-orange-600">PLACE ORDER</Button>
+                      <Button varient="outline" className="w-full bg-orange-400 ">
+                        <Link to="/products" >Countinue Shopping</Link>
+                      </Button>
+                    </div>
+                    <div className='text-sm text-muted-foreground pt-4'>
+                      <p>* Free shipping on orders over 299</p>
+                      <p>* 30-days return policy</p>
+                      <p>* Secure checkout with SSL encryption</p>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
+
+          </div> : <div className='flex flex-col items-center justify-center min-h-[60vh] p-6 text-center'>
+            <div className='bg-orange-100 p-6 rounded-full'>
+              <ShoppingCart className='w-16 h-16 text-orange-600' />
 
             </div>
-            <div>
-              <Card className="w-[400px]">
-                <CardHeader>
-                  <CardTitle>
-                    Order Sumarry
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="space-y-4">
-                  <div className='flex justify-between'>
-                    <span>Subtotal ({cart?.items?.length} items)</span>
-                    <span>₹{cart?.totalPrice?.toLocaleString('en-IN')}</span>
-                  </div>
-                  <div className='flex justify-between'>
-                    <span>Shipping</span>
-                    <span>₹{shipping}</span>
-                  </div>
-                  <div className='flex justify-between'>
-                    <span>Tax(5%)</span>
-                    <span>₹{tax}</span>
-                  </div>
-
-                </CardContent>
-              </Card>
-            </div>
+            <h2 className='mt-6 text-2xl font-bold text-gray-800'>Your Cart is Empty</h2>
+            <p className='mt-2 text-gray-600'>Looks like you haven't added anything to your cart yet</p>
+            <Button onClick={() => navigate("/products")} className="mt-6 bg-orange-600 text-white rounded-xl hover:bg-orange-700 cursor-pointer ">Start Shopping</Button>
           </div>
-
-        </div> : <div></div>
       }
 
     </div>
