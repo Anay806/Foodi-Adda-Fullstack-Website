7d6ba096577ea6bb973b20fce4a8ca272dbb7fa2
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-05-03 22:48:43 +0530

    Adding Dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,16 @@
+import Sidebar from '@/components/Sidebar'
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className='flex '>
+      <Sidebar />
+      <div className='flex-1'>
+        <Outlet />
+
+      </div>
+
     </div>
   )
 }
--- Frontend/src/pages/admin/AddProduct.jsx
+++ Frontend/src/pages/admin/AddProduct.jsx
@@ -1,9 +1,50 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 const AddProduct = () => {
   return (
-    <div>
-      Admin Add product
+    <div className='pl-[350px] py-10 pr-20 mx-auto px-4 bg-gray-100'>
+      <Card className='w-full my-20'>
+        <CardHeader>
+          <CardTitle> Add Product </CardTitle>
+          <CardDescription>Enter Product details below</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className='flex flex-col gap-2'>
+            <div className='grid gap-2'>
+              <Label>Product Name</Label>
+              <Input type='text' name='productName' placeholder='Pizza' required></Input>
+
+            </div>
+            <div className='grid gap-2'>
+              <Label>Price</Label>
+              <Input type='text' name='productPrice' placeholder='Burger' required></Input>
+
+            </div>
+            <div className='grid grid-cols-2 gap-4'>
+              <div className='grid gap-2'>
+                <Label>Brand</Label>
+                <Input type='text' name='brand' placeholder='Brand' required></Input>
+              </div>
+              <div className='grid gap-2'>
+                <Label>Category</Label>
+                <Input type='text' name='category' placeholder='category' required></Input>
+              </div>
+            </div>
+
+            <div className='grid-cols-2'>
+              <div className='flex items-center'>
+                <Label>Description</Label>
+              </div>
+              <Textarea name='productDisc' placeholder="Enter brief description of your product"></Textarea>
+            </div>
+
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
